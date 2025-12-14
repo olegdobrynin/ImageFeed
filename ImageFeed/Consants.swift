dc65a4d4069d5f04fcdee00e65fc5810c2b8e4f2
Oleg <dobryi95@gmail.com>
2025-12-14 19:35:29 +0300

    add constants

---
+++ ImageFeed/Consants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Consants {
+    static let accessKey = "8ohPPAS2Qf-bxAsj0xu9oqm7V01_NwkiQPGnVEBzLVc"
+    static let secretKey = "64ZfX7Xdz5JDlnAf6Ak_bmrUOUe0sMdSKjAJ6R2bJJA"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
