373a3d07241e388556e02e1c871143d6b04b431e
Oleg <dobryi95@gmail.com>
2026-01-10 20:40:09 +0300

    fix force-unwrap

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -5,5 +5,5 @@ enum Constants {
     static let secretKey = "64ZfX7Xdz5JDlnAf6Ak_bmrUOUe0sMdSKjAJ6R2bJJA"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
 }
